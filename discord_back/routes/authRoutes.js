7bf08b565febd2df750291f369e1edfc575687c1
kamel.bouyacoub <kamel.BOUYACOUB@conix.fr>
2022-05-07 12:16:03 +0200

    Add schema validators

---
+++ discord_back/routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const authControllers = require("../controllers/auth/authControllers");
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+
+const registerSchema = Joi.object({
+    username : Joi.string().min(3).max(12),
+    password : Joi.string().min(6).max(12),
+    mail: Joi.string().email()
+});
+
+const loginSchema = Joi.object({
+    password : Joi.string().min(6).max(12),
+    mail: Joi.string().email()
+})
+
+router.post("/register",validator.body(registerSchema), authControllers.controllers.postRegister);
+router.post("/login", validator.body(loginSchema),authControllers.controllers.postLogin);
+
+module.exports = router;
